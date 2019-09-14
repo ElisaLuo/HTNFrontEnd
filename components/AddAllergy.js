55c12410cd362c715da8369f452ff60b889e4b8d
Elisa Luo <ElisaLuo@users.noreply.github.com>
2019-09-14 16:45:03 -0400

    finished add allergies page

--- components/AddAllergy.js
+++ components/AddAllergy.js
@@ -1,17 +1,125 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  AsyncStorage,
+  Button,
+  TextInput
+} from "react-native";
 
-export default class AddAllergy extends React.Component {
-  render(){
-      return(
-        <View>
-            <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-        </View>
-      )
+export default class TodoList extends Component {
+  state = {
+    tasks: [],
+    text: ""
+  };
+
+  changeTextHandler = text => {
+    this.setState({ text: text });
+  };
+
+  addTask = () => {
+    let notEmpty = this.state.text.trim().length > 0;
+
+    if (notEmpty) {
+      this.setState(
+        prevState => {
+          let { tasks, text } = prevState;
+          return {
+            tasks: tasks.concat({ 
+                key: tasks.length, 
+                text: text }),
+            text: ""
+          };
+        },
+        () => Tasks.save(this.state.tasks)
+      );
+    }
+  };
+
+  deleteTask = i => {
+    this.setState(
+      prevState => {
+        let tasks = prevState.tasks.slice();
+        tasks.splice(i, 1);
+        return { tasks: tasks };
+      },
+      () => Tasks.save(this.state.tasks)
+    );
+  };
+
+  componentDidMount() {
+    Tasks.all(tasks => this.setState({ tasks: tasks || [] }));
+  }
+
+  render() {
+    return (
+      <View
+        style={styles.container}
+      >
+          <TextInput
+          style={styles.textInput}
+          onChangeText={this.changeTextHandler}
+          onSubmitEditing={this.addTask}
+          value={this.state.text}
+          placeholder="Add Tasks"
+          returnKeyType="done"
+          returnKeyLabel="done"
+        />
+        <FlatList
+          style={styles.list}
+          data={this.state.tasks}
+          renderItem={({ item, index }) =>
+            <View>
+              <View style={styles.listItemCont}>
+                <Text style={styles.listItem}>
+                  {item.text}
+                </Text>
+                <Button title="X" onPress={() => this.deleteTask(index)} />
+              </View>
+              <View style={styles.hr} />
+            </View>}
+        />
+        
+      </View>
+    );
   }
 }
+
+let Tasks = {
+  convertToArrayOfObject(tasks, callback) {
+    return callback(
+      tasks ? tasks.split("||").map((task, i) => ({ key: i, text: task })) : []
+    );
+  },
+  convertToStringWithSeparators(tasks) {
+    return tasks.map(task => task.text).join("||");
+  },
+  all(callback) {
+    return AsyncStorage.getItem("TASKS", (err, tasks) =>
+      this.convertToArrayOfObject(tasks, callback)
+    );
+  },
+  save(tasks) {
+    AsyncStorage.setItem("TASKS", this.convertToStringWithSeparators(tasks));
+  }
+};
+
 const styles = StyleSheet.create({
-    text:{
-        fontSize: 30
-    }
-  });
+  container: {
+    paddingTop: 20
+  },
+  listItem: {
+    fontSize: 18
+  },
+  hr: {
+    height: 1,
+  },
+  listItemCont: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  }
+});
